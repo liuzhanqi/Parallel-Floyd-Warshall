ea15c3234a67f2d20b4131ebd920ad03229c5813
cly753 <cly753@gmail.com>
2015-11-13 14:19:46 +0800

    add the clean code which is put in the report

--- CUDA/3_lab_4011/code/apsp/par_apsp_naive.cu
+++ CUDA/3_lab_4011/code/apsp/par_apsp_naive.cu
@@ -1,18 +1,18 @@
-#define BLOCK_SIZE 128
+#define BLOCK_SIZE 256
 
-__global__ void apspKernel(int N, int k, int *global_mat) {
+__global__ void apspKernel(const int N, const int k, int *global_mat) {
     const int ij = blockIdx.x * BLOCK_SIZE + threadIdx.x;
     const int i = ij / N;
     const int ik = i * N + k;
     const int kj = ij + (k - i) * N;
     const int dik = global_mat[ik];
     const int dkj = global_mat[kj];
+
     if (dik != -1 && dkj != -1) {
         const int sum = dik + dkj;
         int& dij = global_mat[ij];
-        if (dij == -1 || dij > sum) {
+        if (dij == -1 || dij > sum)
             dij = sum;
-        }
     }
 }
 
@@ -29,3 +29,73 @@ void par_apsp(int N, int *mat) {
 
     cudaMemcpy(mat, global_mat, size, cudaMemcpyDeviceToHost);
 }
+
+// __global__ void apspKernel(int N, int k, int *g_idata, int *g_odata) {
+//     // access thread id
+//     const unsigned int tid = threadIdx.x;
+//     // access block id
+//     const unsigned int bid = blockIdx.x;
+//     // access number of threads in this block
+//     const unsigned int bdim = blockDim.x;
+
+//     const unsigned int i = (bid * bdim + tid)/N;
+//     const unsigned int j = (bid * bdim + tid)%N;
+
+//     if (g_idata[i*N+k] == -1 || g_idata[k*N+j] == -1) g_odata[i*N+j] = g_idata[i*N+j];
+//     else if (g_idata[i*N+j] == -1) g_odata[i*N+j] = g_idata[i*N+k]+g_idata[k*N+j];
+//     else g_odata[i*N+j] = min(g_idata[i*N+j], g_idata[i*N+k]+g_idata[k*N+j]);
+// }
+
+// void par_apsp(int N, int *mat) {
+//     //copy mat from host to device memory d_mat
+//     int* d_mat;
+//     int* d_mat_out;
+//     int size = sizeof(int) * N * N;
+//     cudaMalloc((void**) &d_mat, size);
+//     cudaMemcpy(d_mat, mat, size, cudaMemcpyHostToDevice);
+//     //allocate matrix to hold temporary result of each iteration to avoid race condition.
+//     cudaMalloc((void**) &d_mat_out, size);
+
+//     for (int k = 0; k < N; k++) {
+//         apspKernel<<<ceil(N*N/256), 256>>>(N, k, d_mat, d_mat_out);
+//         //copy the temporary result back to the matrix
+//         cudaMemcpy(d_mat, d_mat_out, size, cudaMemcpyDeviceToDevice);
+//     }
+
+//     cudaMemcpy(mat, d_mat, size, cudaMemcpyDeviceToHost);
+// }
+
+// __global__ void apspKernel(int N, int k, int *g_data) {
+//     const int tid = threadIdx.x;
+//     const int bid = blockIdx.x;
+//     const int bdim = blockDim.x;
+
+//     const int ij = bid * bdim + tid;
+    
+//     const int i = ij / N;
+//     const int j = ij % N;
+
+//     const int ik = i * N + k;
+//     const int kj = k * N + j;
+//     const int dik = g_data[ik];
+//     const int dkj = g_data[kj];
+    
+//     if (dik != -1 && dkj != -1) {
+//         const int sum = dik + dkj;
+//         int& dij = g_data[ij];
+//         if (dij == -1 || dij > sum)
+//             dij = sum;
+//     }
+// }
+
+// void par_apsp(int N, int *mat) {
+//     int* d_mat;
+//     int size = sizeof(int) * N * N;
+//     cudaMalloc((void**) &d_mat, size);
+//     cudaMemcpy(d_mat, mat, size, cudaMemcpyHostToDevice);
+    
+//     for (int k = 0; k < N; k++)
+//         apspKernel<<<ceil(N*N/256), 256>>>(N, k, d_mat);
+
+//     cudaMemcpy(mat, d_mat, size, cudaMemcpyDeviceToHost);
+// }
--- CUDA/3_lab_4011/code/apsp/par_blocked_apsp.cu
+++ CUDA/3_lab_4011/code/apsp/par_blocked_apsp.cu
@@ -255,3 +255,214 @@ void par_blocked_apsp(int N, int *mat) {
 #endif
 
 }
+
+
+
+
+
+// __global__ void process_type_1_tile_kernel(int N, int *mat_device, int start)
+// {
+// 	int i = threadIdx.y * THREAD_SIZE;
+// 	int j = threadIdx.x;
+
+// 	__shared__ int tile[TILE_SIZE][TILE_SIZE];
+// 	int mat_i = start + i;
+// 	int mat_j = start + j;
+// 	int mat_ij = mat_i * N + mat_j;
+// 	for (int t = 0; t < THREAD_SIZE; t++) // load tile into shared memory
+// 		tile[i + t][j] = mat_device[mat_ij + t * N];
+
+// 	for (int k = 0; k < TILE_SIZE; k++) {
+// 		for (int t = 0; t < THREAD_SIZE; t++) {
+// 			__syncthreads();
+
+// 			int dik = tile[i + t][k];
+// 			int dkj = tile[k][j];
+// 			if (dik != -1 && dkj != -1) {
+// 				int d = dik + dkj;
+// 				int& dij = tile[i + t][j];
+// 				if (dij == -1 || dij > d)
+// 					dij = d;
+// 			}	
+// 		}
+// 	}
+
+// 	for (int t = 0; t < THREAD_SIZE; t++) // store tile back to global memory
+// 		tile[i + t][j] = mat_device[mat_ij + t * N];
+// }
+
+// inline void process_type_1_tile(int N, int *mat_device, int n_tile, int start)
+// {
+// 	dim3 dimBlock(1, 1, 1);
+// 	dim3 dimGrid(TILE_SIZE, TILE_SIZE / THREAD_SIZE, 1);
+// 	process_type_1_tile_kernel<<<dimBlock, dimGrid>>>(N, mat_device, start);
+// }
+
+// __global__ void process_type_2_tile_kernel(int N, int *mat_device, int start)
+// {
+// 	int i = threadIdx.y * THREAD_SIZE;
+// 	int j = threadIdx.x;
+
+// 	__shared__ int prime[TILE_SIZE][TILE_SIZE];
+// 	int prime_mat_i = start + i;
+// 	int prime_mat_j = start + j;
+// 	int prime_mat_ij = prime_mat_i * N + prime_mat_j;
+// 	for (int t = 0; t < THREAD_SIZE; t++) // load tile into shared memory
+// 		prime[i + t][j] = mat_device[prime_mat_ij + t * N];
+// 	__syncthreads();
+
+// 	__shared__ int tile[TILE_SIZE][TILE_SIZE];
+// 	int mat_i;
+// 	int mat_j;
+// 	if (blockIdx.x == 0) {
+// 		mat_i = start + i;
+// 		mat_j = blockIdx.y * TILE_SIZE + j;
+// 		if (mat_j >= start)
+// 			mat_j += TILE_SIZE;
+// 	}
+// 	else {
+// 		mat_i = blockIdx.y * TILE_SIZE + i;
+// 		if (mat_i >= start)
+// 			mat_i += TILE_SIZE;
+// 		mat_j = start + j;
+// 	}
+// 	int mat_ij = mat_i * N + mat_j;
+	
+// 	for (int t = 0; t < THREAD_SIZE; t++) // load tile into shared memory
+// 		tile[i + t][j] = mat_device[mat_ij + t * N];
+
+// 	for (int k = 0; k < TILE_SIZE; k++) {
+// 		for (int t = 0; t < THREAD_SIZE; t++) {
+// 			int dik = prime[i + t][k];
+// 			int dkj = prime[k][j];
+// 			if (dik != -1 && dkj != -1) {
+// 				int d = dik + dkj;
+// 				int& dij = tile[i + t][j];
+// 				if (dij == -1 || dij > d)
+// 					dij = d;
+// 			}
+// 		}
+// 	}
+
+// 	for (int t = 0; t < THREAD_SIZE; t++) // store tile back to global memory
+// 		mat_device[mat_ij + t * N] = tile[i + t][j];
+// }
+
+// inline void process_type_2_tile(int N, int *mat_device, int n_tile, int start)
+// {
+// 	dim3 dimBlock(2, n_tile - 1, 1); // 0: horizontal ---, 1: vertical |||
+// 	dim3 dimGrid(TILE_SIZE, TILE_SIZE / THREAD_SIZE, 1);
+// 	process_type_2_tile_kernel<<<dimBlock, dimGrid>>>(N, mat_device, start);
+// }
+
+// __global__ void process_type_3_tile_kernel(int N, int *mat_device, int start)
+// {
+// #ifdef THREAD_DO_VERTICAL
+// 	int i = threadIdx.y * THREAD_SIZE;
+// 	int j = threadIdx.x;
+// #else
+// 	int i = threadIdx.y;
+// 	int j = threadIdx.x * THREAD_SIZE;
+// #endif
+
+// 	__shared__ int hor[TILE_SIZE][TILE_SIZE];
+// 	int hor_mat_i = start + i;
+// 	int hor_mat_j = start + j;
+// 	if (hor_mat_j >= start)
+// 		hor_mat_j += TILE_SIZE;
+// 	int hor_mat_ij = hor_mat_i * TILE_SIZE + hor_mat_j;
+// 	for (int t = 0; t < THREAD_SIZE; t++) // load tile into shared memory
+// #ifdef THREAD_DO_VERTICAL
+// 		hor[i + t][j] = mat_device[hor_mat_ij + t * N];
+// #else
+// 		hor[i][j + t] = mat_device[hor_mat_ij + t];
+// #endif
+
+// 	__shared__ int ver[TILE_SIZE][TILE_SIZE];
+// 	int ver_mat_i = start + i;
+// 	if (ver_mat_i >= start)
+// 		ver_mat_i += TILE_SIZE;
+// 	int ver_mat_j = start + j;
+// 	int ver_mat_ij = ver_mat_i * TILE_SIZE + ver_mat_j;
+// 	for (int t = 0; t < THREAD_SIZE; t++) // load tile into shared memory
+// #ifdef THREAD_DO_VERTICAL
+// 		hor[i + t][j] = mat_device[ver_mat_ij + t * N];
+// #else
+// 		hor[i][j + t] = mat_device[ver_mat_ij + t];
+// #endif
+// 	__syncthreads();
+
+// 	__shared__ int tile[TILE_SIZE][TILE_SIZE];
+// 	int mat_i = blockIdx.y * TILE_SIZE + i;
+// 	int mat_j = blockIdx.x * TILE_SIZE + j;
+// 	if (mat_i >= start)
+// 		mat_i += TILE_SIZE;
+// 	if (mat_j >= start)
+// 		mat_j += TILE_SIZE;
+// 	int mat_ij = mat_i * N + mat_j;
+// 	for (int t = 0; t < THREAD_SIZE; t++) // load tile into shared memory
+// #ifdef THREAD_DO_VERTICAL
+// 		tile[i + t][j] = mat_device[mat_device + t * N];
+// #else
+// 		tile[i][j + t] = mat_device[mat_device + t];
+// #endif
+
+// 	for (int k = 0; k < TILE_SIZE; k++) {
+// 		for (int t = 0; t < THREAD_SIZE; t++) {
+// #ifdef THREAD_DO_VERTICAL
+// 			int dik = hor[i + t][k];
+// 			int dkj = ver[k][j];
+// #else
+// 			int dik = hor[i][k];
+// 			int dkj = ver[k][j + t];
+// #endif
+// 			if (dik != -1 && dkj != -1) {
+// 				int d = dik + dkj;
+// #ifdef THREAD_DO_VERTICAL
+// 				int& dij = tile[i + t][j];
+// #else
+// 				int& dij = tile[i][j + t];
+// #endif
+// 				if (dij == -1 || dij > d)
+// 					dij = d;
+// 			}
+// 		}
+// 	}
+
+// 	for (int t = 0; t < THREAD_SIZE; t++) // store tile back to global memory
+// #ifdef THREAD_DO_VERTICAL
+// 		mat_device[mat_ij + t * N] = tile[i + t][j];
+// #else
+// 		mat_device[mat_ij + t] = tile[i][j + t];
+// #endif
+// }
+
+// inline void process_type_3_tile(int N, int *mat_device, int n_tile, int start)
+// {
+// 	dim3 dimBlock(n_tile - 1, n_tile - 1, 1);
+// #ifdef THREAD_DO_VERTICAL
+// 	dim3 dimGrid(TILE_SIZE, TILE_SIZE / THREAD_SIZE, 1);
+// #else
+// 	dim3 dimGrid(TILE_SIZE / THREAD_SIZE, TILE_SIZE, 1);
+// #endif
+// 	process_type_3_tile_kernel<<<dimBlock, dimGrid>>>(N, mat_device, start);
+// }
+
+// void par_blocked_apsp(int N, int *mat)
+// {
+//     int *mat_device;
+//     size_t size = N * N * sizeof(int);
+//     cudaMalloc(&mat_device, size);
+//     cudaMemcpy(mat_device, mat, size, cudaMemcpyHostToDevice);
+
+//     int n_tile = N / TILE_SIZE;
+//     for (int i = 0; i < n_tile; i++) {
+//     	int start = i * TILE_SIZE;
+//         process_type_1_tile(N, mat_device, n_tile, start);
+//         process_type_2_tile(N, mat_device, n_tile, start);
+//         process_type_3_tile(N, mat_device, n_tile, start);
+//     }
+
+//     cudaMemcpy(mat, mat_device, size, cudaMemcpyDeviceToHost);
+//     cudaFree(mat_device);
+// }
